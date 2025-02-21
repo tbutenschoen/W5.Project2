7fc902e83466c0816a3541803092c009e9afcc69
Tayler <tuq24156@temple.edu>
2025-02-20 23:58:06 -0500

    declared variables, added event listeners, getElementById, getting err 404 tho

--- MusicTools.js
+++ MusicTools.js
@@ -1,15 +1,40 @@
-MIDI2freq = function (midiPitch) {
-  return 440 * Math.pow(2, (midiPitch - 69) / 12);
-};
+function midiToFreq(midiPitch) {
+  let f1 = 440 * Math.pow(2, (midiPitch - 69) / 12);
+  return f1;
+}
 
-freq2MIDI = function (freq) {
-  return 69 + 12 * Math.log2(freq / 440);
-};
+function freqToMidi(freq) {
+  let f2 = 69 + 12 * Math.log2(freq / 440);
+  return f2;
+}
 
-dBFS2a = function (dBFS) {
-  return Math.pow(10, dBFS / 20);
-};
+function dbfsToLinear(dBFS) {
+  let f3 = Math.pow(10, dBFS / 20);
+  return f3;
+}
 
-a2dBFS = function (linear) {
-  return 20 * Math.log10(linear);
-};
+function linearToDbfs(linear) {
+  let f4 = 20 * Math.log10(linear);
+  return f4;
+}
+
+let midiPitch = document.getElementById("midiInput");
+let freq = document.getElementById("freqInput");
+let dBFS = document.getElementById("dbfsInput");
+let linear = document.getElementById("linearInput");
+let f1 = document.getElementById("freqResult");
+let f2 = document.getElementById("midiResult");
+let f3 = document.getElementById("linearResult");
+let f4 = document.getElementById("dbfsResult");
+
+const button = document.getElementById("midiToFreq");
+button.addEventListener("click", midiToFreq);
+
+const button2 = document.getElementById("freqToMidi");
+button.addEventListener("click", freqToMidi);
+
+const button3 = document.getElementById("dbfsToLinear");
+button.addEventListener("click", dbfsToLinear);
+
+const button4 = document.getElementById("linearToDbfs");
+button.addEventListener("click", linearToDbfs);
