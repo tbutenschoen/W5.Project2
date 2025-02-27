7ef4a4b242ba8dc057ce4708f3808b89bd02c995
Tayler <tuq24156@temple.edu>
2025-02-26 20:21:21 -0500

    got rid of docelement

--- MusicTools.js
+++ MusicTools.js
@@ -3,22 +3,25 @@ const myMusicTools = {
   freq: document.getElementById("freqInput"),
   dBFS: document.getElementById("dbfsInput"),
   linear: document.getElementById("linearInput"),
+ 
 
   midiToFreq: function (midiPitch) {
-    return document.getElementById("freqResult") = 440 * Math.pow(2, (midiPitch - 69) / 12);
+    return 440 * Math.pow(2, (midiPitch - 69) / 12);
   },
 
   freqToMidi: function (freq) {
-    return document.getElementById("midiResult") = 69 + 12 * Math.log2(freq / 440);
+    return 69 + 12 * Math.log2(freq / 440);
   },
 
   dbfsToLinear: function (dBFS) {
-    return document.getElementById("linearResult") = Math.pow(10, dBFS / 20);
+    return Math.pow(10, dBFS / 20);
   },
 
   linearToDbfs: function (linear) {
-    return document.getElementById("dbfsResult") = 20 * Math.log10(linear);
+    return  20 * Math.log10(linear);
   },
+
+  
 
   
 };
